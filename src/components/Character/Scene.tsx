fedf1a3a0ee784e3f61eaa893c9ffe2fe8e93100
Jatin Yadav <jatinyadav5004@gmail.com>
2026-07-21 22:54:25 +0530

    de

--- src/components/Character/Scene.tsx
+++ src/components/Character/Scene.tsx
@@ -38,8 +38,9 @@ const Scene = () => {
       canvasDiv.current.appendChild(renderer.domElement);
 
       const camera = new THREE.PerspectiveCamera(14.5, aspect, 0.1, 1000);
-      camera.position.set(0, 13.1, 31);
-      camera.zoom = 0.9;
+      camera.position.z = 10;
+      camera.position.set(0, 13.1, 24.7);
+      camera.zoom = 1.1;
       camera.updateProjectionMatrix();
 
       let headBone: THREE.Object3D | null = null;
